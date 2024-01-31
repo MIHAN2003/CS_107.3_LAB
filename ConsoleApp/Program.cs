0ffb59cea6c6cb0198f71166627143c3c94a087e
MIHAN2003 <137778400+MIHAN2003@users.noreply.github.com>
2024-01-31 14:50:28 +0530

    CHANGE

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,15 +1,33 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace ConsoleApp
+namespace ConsoleApp4
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            Console.WriteLine("Enter the number of Fibonacci term:");
+            int n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Fibonacci number:");
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(Fibonacci(i) + " ");
+            }
+            Console.ReadLine();
+        }
+
+        static int Fibonacci(int n)
+        {
+            if (n <= 1)
+            {
+                return n;
+            }
+            else
+            {
+                return Fibonacci(n - 1) + Fibonacci(n - 2);
+            }
         }
     }
+
 }
