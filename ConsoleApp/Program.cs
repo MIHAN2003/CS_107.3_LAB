ee1745ace7471a8cf460e60562322df6af12f92f
MIHAN2003 <137778400+MIHAN2003@users.noreply.github.com>
2024-02-02 09:59:39 +0530

    CHANGE

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -8,20 +8,53 @@ namespace q1
 {
     internal class Program
     {
-        static void Main()
-        {
-            Console.WriteLine("Enter the number: ");
-            int num = int.Parse(Console.ReadLine());
 
-            for (int i = 1; i < 13; i++)
-            {
-                int res = num * i;
-                Console.WriteLine(num + "x" + i + "=" + res);
-            }
-            Console.ReadLine();
+        static void Bal()
+        {
+            Console.WriteLine("your balance is xxxxx.");
+        }
+
+        static void Dep()
+        {
+            Console.WriteLine("Enter the money through counter thank you.");
+        }
+
+        static void Widraw()
+        {
+            Console.WriteLine("Enter the Amount you want to withdraw");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine(num1 + " amout is ready to withdarw");
 
         }
 
+        static void Main()
+        {
+            Console.WriteLine("How do you want to proceed?");
+            Console.WriteLine("type '1' for check the balance");
+            Console.WriteLine("type '2' for deposit money");
+            Console.WriteLine("type '3' for withdraw money");
+            int num = int.Parse(Console.ReadLine());
+
+            if (num == 1)
+            {
+                Bal();
+            }
+            else if (num == 2)
+            {
+                Dep();
+            }
+            else if (num == 3)
+            {
+                Widraw();
+            }
+            else
+            {
+                Console.WriteLine("Invalid command");
+            }
+
+            Console.ReadLine();
+
+        }
 
 
     }
