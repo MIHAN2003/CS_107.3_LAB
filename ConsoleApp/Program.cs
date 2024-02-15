f1c3323478cec3d9ccb2d1bbd051cc45ea28d301
MIHAN2003 <137778400+MIHAN2003@users.noreply.github.com>
2024-02-15 14:20:42 +0530

    change

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -4,12 +4,45 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp
+namespace Q1
 {
+    public class EncapData
+    {
+        public double Radius;
+        private double Pi = 3.14;
+
+        public double radius
+        {
+            get{ return Radius; }
+            set{ Radius = value; }
+        }
+        public double Area()
+        { 
+            double area = Pi * Radius * Radius;
+            return area;
+        }
+        public double Circumference()
+        {
+            double circum = Pi * Radius * Radius;
+            return circum;
+        }
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
+
+            EncapData data = new EncapData();
+        Console.WriteLine("Enter the Radius of the circle");
+        double radius = double.Parse(Console.ReadLine());
+
+        data.radius = radius;
+
+        Console.WriteLine($"Area of the circle is  {data.Area()}");
+        Console.WriteLine($"Circumference of the circle is  {data.Circumference()}");
+
+        Console.ReadLine();
+
         }
     }
 }
