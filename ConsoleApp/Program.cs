1ae85548dcba21ab5be17955320d4232eedff350
MIHAN2003 <137778400+MIHAN2003@users.noreply.github.com>
2024-02-15 14:34:08 +0530

    CHANGE

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -6,22 +6,35 @@ using System.Threading.Tasks;
 
 namespace q1
 {
-        
-    public class BankAccout
+    public class BankAccount
     {
-       
+        public int AccoutNumber;
+        public int Balance;
 
 
+
+        public void Deposit(int amount)
+        {
+            Balance = Balance + amount;
+        }
     }
-
     internal class Program
     {
-
-
-        static void Main()
+        public static void Main(string[] args)
         {
-          
+            BankAccount account1 = new BankAccount();
+
+            Console.WriteLine("Enter you Account number:");
+            account1.AccoutNumber = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the Amount you wish to deposit:");
+            int amount = int.Parse(Console.ReadLine());
+            account1.Deposit(amount);
+
+            Console.WriteLine($"In {account1.AccoutNumber} account's updated balance is {account1.Balance}.");
+            Console.ReadLine();
         }
+
 
 
     }
