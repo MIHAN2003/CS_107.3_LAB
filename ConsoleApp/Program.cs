15bd35347777efc21303d71d21a2f618d9bd4ffe
MIHAN2003 <137778400+MIHAN2003@users.noreply.github.com>
2024-02-15 14:35:51 +0530

    CHANGE

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -6,33 +6,31 @@ using System.Threading.Tasks;
 
 namespace q1
 {
-    public class BankAccount
+    public class TempuratureTracker
     {
-        public int AccoutNumber;
-        public int Balance;
+        public int[] temp = new int[7];
 
 
-
-        public void Deposit(int amount)
-        {
-            Balance = Balance + amount;
-        }
     }
     internal class Program
     {
         public static void Main(string[] args)
         {
-            BankAccount account1 = new BankAccount();
+            TempuratureTracker obj1 = new TempuratureTracker();
 
-            Console.WriteLine("Enter you Account number:");
-            account1.AccoutNumber = int.Parse(Console.ReadLine());
+            for (int i = 0; i < 7; i++)
+            {
+                Console.WriteLine("Enter the Tempurature:");
+                obj1.temp[i] = int.Parse(Console.ReadLine());
+            }
 
-            Console.WriteLine("Enter the Amount you wish to deposit:");
-            int amount = int.Parse(Console.ReadLine());
-            account1.Deposit(amount);
-
-            Console.WriteLine($"In {account1.AccoutNumber} account's updated balance is {account1.Balance}.");
+            Console.WriteLine("Weekly Tempurature Report");
+            for (int i = 0; i < 7; i++)
+            {
+                Console.WriteLine($"{obj1.temp[i]} celcius");
+            }
             Console.ReadLine();
+
         }
 
 
