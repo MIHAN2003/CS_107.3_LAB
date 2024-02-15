4910af55aac661ba97af4c87eda5fde1c4f37e33
MIHAN2003 <137778400+MIHAN2003@users.noreply.github.com>
2024-02-15 14:32:48 +0530

    change

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -6,22 +6,31 @@ using System.Threading.Tasks;
 
 namespace q1
 {
-        
-    public class BankAccout
+    public class Book
     {
-       
+        public string Title;
+        public string Authour;
+
 
 
     }
-
     internal class Program
     {
-
-
-        static void Main()
+        public static void Main(string[] args)
         {
-          
+            Book book1 = new Book();
+
+            Console.WriteLine("Enter the Book Title:");
+            book1.Title = Console.ReadLine();
+
+            Console.WriteLine("Enter the Authour of this book:");
+            book1.Authour = Console.ReadLine();
+
+            Console.WriteLine("title of the book is " + book1.Title);
+            Console.WriteLine("authour of the book is " + book1.Authour);
+            Console.ReadLine();
         }
+
 
 
     }
