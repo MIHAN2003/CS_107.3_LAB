03a9a3a39d476fd6400a9424601eba223ee9fc6d
MIHAN2003 <137778400+MIHAN2003@users.noreply.github.com>
2024-02-15 14:42:52 +0530

    CHANGE

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -6,32 +6,59 @@ using System.Threading.Tasks;
 
 namespace q1
 {
-    public class BankAccount
+    public class Employee
     {
-        public int AccoutNumber;
-        public int Balance;
+        private int EmployeeID;
+        private string FullName;
+        public double Salary;
 
-
-
-        public void Deposit(int amount)
+        public Employee(int employeeID, string name, double salary)
         {
-            Balance = Balance + amount;
+            EmployeeID = employeeID;
+            FullName = name;
+            Salary = salary;
         }
+
+
+        public int employeeID => EmployeeID;//read-only property for employeeID
+
+        public string fullName
+        {
+            get
+            {
+                return FullName;
+            }
+            set
+            {
+                FullName = value;
+            }
+        }
+
+        public void DisplayEmployeeInfo()
+        {
+            Console.WriteLine($"Employee's ID: {employeeID}");
+            Console.WriteLine($"Employee's Name: {FullName}");
+            Console.WriteLine($"Employee's Salary: {Salary}");
+        }
+
+
     }
     internal class Program
     {
         public static void Main(string[] args)
         {
-            BankAccount account1 = new BankAccount();
+            Employee emp1 = new Employee(101, "John Doe", 50000);
+            Console.WriteLine("Enter the new Name of the Employee: ");
+            emp1.fullName = Console.ReadLine();
 
-            Console.WriteLine("Enter you Account number:");
-            account1.AccoutNumber = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the new Salary of the Emplyee: ");
+            emp1.Salary = double.Parse(Console.ReadLine());
 
-            Console.WriteLine("Enter the Amount you wish to deposit:");
-            int amount = int.Parse(Console.ReadLine());
-            account1.Deposit(amount);
+            emp1.DisplayEmployeeInfo();
 
-            Console.WriteLine($"In {account1.AccoutNumber} account's updated balance is {account1.Balance}.");
+
+
+
             Console.ReadLine();
         }
 
